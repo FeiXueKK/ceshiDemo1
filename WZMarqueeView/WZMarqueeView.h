00b57c3cb36f86280d3d4340bdf757099c45dcf2
xuehuawanlipiao <xuehua@xuehuadeMac.local>
2014-09-08 22:31:50 +0800

    Release 1.0.0.

---
+++ WZMarqueeView/WZMarqueeView.h
@@ -0,0 +1,13 @@
+//
+//  WZMarqueeView.h
+//  WZMarqueeViewDemo
+//
+//  Created by xuehua on 14-9-8.
+//  Copyright (c) 2014年 xuehua. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WZMarqueeView : UIView
+
+@end
---
+++ WZMarqueeView/WZMarqueeView.m
@@ -0,0 +1,31 @@
+//
+//  WZMarqueeView.m
+//  WZMarqueeViewDemo
+//
+//  Created by xuehua on 14-9-8.
+//  Copyright (c) 2014年 xuehua. All rights reserved.
+//
+
+#import "WZMarqueeView.h"
+
+@implementation WZMarqueeView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
